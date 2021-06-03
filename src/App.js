0bf3c0e70bf7a36153f923212223d537d04bfa39
Rafael Schiefelbein <80975453+Rafa3lSch@users.noreply.github.com>
2021-06-03 11:19:58 +0200

    add css

--- src/App.js
+++ src/App.js
@@ -1,71 +1,76 @@
 import React, { useState } from "react";
 import "./App.css";
 
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
   const [tasks, setTasks] = useState([]);
   const [newTaskInput, setNewTaskInput] = useState("");
 
   return (
-    <div>
-      <h1>To do list</h1>
+    <>
+      <Header />
+      <div className="container">
+        {tasks.map((task, index) => {
+          return (
+            <div>
+              <input
+                checked={task.isDone}
+                type="checkbox"
+                onClick={() => {
+                  const newTasks = [...tasks];
 
-      {tasks.map((task, index) => {
-        return (
-          <div>
-            <input
-              checked={task.isDone}
-              type="checkbox"
-              onClick={() => {
-                const newTasks = [...tasks];
+                  if (newTasks[index].isDone === true) {
+                    newTasks[index].isDone = false;
+                  } else {
+                    newTasks[index].isDone = true;
+                  }
 
-                if (newTasks[index].isDone === true) {
-                  newTasks[index].isDone = false;
-                } else {
-                  newTasks[index].isDone = true;
-                }
+                  setTasks(newTasks);
+                }}
+              />
+              <span className={task.isDone === true ? "checked" : ""}>
+                {task.title}
+              </span>
+              <button
+                onClick={() => {
+                  const newTasks = [...tasks];
+                  newTasks.splice(index, 1);
+                  setTasks(newTasks);
+                }}
+              >
+                Supprimer
+              </button>
+            </div>
+          );
+        })}
 
-                setTasks(newTasks);
-              }}
-            />
-            <span className={task.isDone === true ? "checked" : ""}>
-              {task.title}
-            </span>
-            <button
-              onClick={() => {
-                const newTasks = [...tasks];
-                newTasks.splice(index, 1);
-                setTasks(newTasks);
-              }}
-            >
-              Supprimer
-            </button>
-          </div>
-        );
-      })}
-
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          const newTasks = [...tasks];
-          newTasks.push({
-            title: newTaskInput,
-            isDone: false,
-          });
-          setTasks(newTasks);
-          setNewTaskInput("");
-        }}
-      >
-        <input
-          type="text"
-          value={newTaskInput}
-          onChange={(event) => {
-            const value = event.target.value;
-            setNewTaskInput(value);
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            const newTasks = [...tasks];
+            newTasks.push({
+              title: newTaskInput,
+              isDone: false,
+            });
+            setTasks(newTasks);
+            setNewTaskInput("");
           }}
-        />
-        <button type="submit">Add task</button>
-      </form>
-    </div>
+        >
+          <input
+            type="text"
+            value={newTaskInput}
+            onChange={(event) => {
+              const value = event.target.value;
+              setNewTaskInput(value);
+            }}
+          />
+          <button type="submit">Add task</button>
+        </form>
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>
+        Made with{" "}
+        <a href="https://fr.reactjs.org/" target="_blank" rel="noreferrer">
+          React
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://www.linkedin.com/in/rafael-schiefelbein-51127495/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Rafael Sch
+        </a>
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
